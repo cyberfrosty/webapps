0afa86506491aa4effc7783a70b545623acb5eb6
Alan Frost <frosty.alan@gmail.com>
2017-04-21 15:55:26 -0700

    Added EC support functions, clean up pylint warnings

--- crypto.py
+++ crypto.py
@@ -10,14 +10,16 @@ Crypto functions
 
 import base64
 import os
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
+from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.exceptions import InvalidTag, InvalidKey
+from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHash
+from cryptography.exceptions import InvalidTag, InvalidKey, InvalidSignature
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.backends.openssl import backend
 
 def derive_key(password, mcf='', bits=256):
     """ Derive key using PBKDF2 (Password-Based Key Derivation Function2, PKCS #5 v2.0)
@@ -121,6 +123,14 @@ def scrypt_key(password, mcf='', bits=512):
     return '$scrypt$' + str(cost) + '$' + base64.b64encode(salt) + '$' + base64.b64encode(key)
 
 def hkdf_key(key, info, salt=None):
+    """ HKDF (HMAC-based Extract-and-Expand Key Derivation Function)
+    Args:
+        Key material
+        Application specific context information
+        Salt to randomize the KDF’s output, optional, but highly recommended and should be 32 bytes
+    Return:
+        Derived key
+    """
     if salt is None:
         salt = os.urandom(32)
     hkdf = HKDF(
@@ -131,6 +141,132 @@ def hkdf_key(key, info, salt=None):
         backend=default_backend()
     )
     return hkdf.derive(key)
+
+def generate_ec(curve=ec.SECP256R1()):
+    """ Generate an EC keypair with a default curve NIST P-256
+    Args:
+        curve
+    Return:
+        private key
+    """
+    ec_key = ec.generate_private_key(curve, default_backend())
+    return ec_key
+
+def get_ec_public(ec_key):
+    """ Get the EC public key from the private key in PEM format
+    Return:
+        public key in PEM format
+    """
+    public_key = ec_key.public_key()
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    return pem
+
+def load_ec_public(pem):
+    """ Load EC public key from PEM
+    Args:
+        PEM format public key
+    Return:
+        EC public key
+    """
+    try:
+        ec_key = serialization.load_pem_public_key(pem, backend=default_backend())
+    except ValueError as inst:
+        print inst
+    return ec_key
+
+def load_ec_private(pem, password=None):
+    """ Load EC private key from PEM
+    Args:
+        PEM format private key
+    Return:
+        EC private key
+    """
+    try:
+        ec_key = serialization.load_pem_private_key(pem, password, backend=default_backend())
+    except (ValueError, TypeError) as inst:
+        print inst
+    return ec_key
+
+def compute_ecdh_secret(ec_key, peer_public_key):
+    """ Compute the ECDH shared secret
+    Args:
+        Peer's public key
+    Return:
+        shared secret for key derivation
+    """
+    secret = ec_key.exchange(ec.ECDH(), peer_public_key)
+    return secret
+
+def encrypt_ecdh(ec_key, pub_pem, payload, other_info):
+    """ Encrypt a payload using ECDH shared secret to derive AES256-GCM key
+    Args:
+        My private key
+        Peer's public key
+        Payload to encrypt
+        Application specific context information
+    Return:
+        Encrypted payload
+    """
+    peer_public_key = serialization.load_pem_public_key(pub_pem, backend=default_backend())
+    secret = compute_ecdh_secret(ec_key, peer_public_key)
+    ckdf = ConcatKDFHash(
+        algorithm=hashes.SHA256(), length=32, otherinfo=other_info, backend=default_backend())
+    key = ckdf.derive(secret)
+    nonce = os.urandom(12)
+    ciphertext = nonce + encrypt_aes_gcm(key, nonce, payload, other_info)
+    return ciphertext
+
+def decrypt_ecdh(ec_key, pub_pem, payload, other_info):
+    """ Decrypt a payload using ECDH shared secret to derive AES256-GCM key
+    Args:
+        My private key
+        Peer's public key
+        Payload to decrypt
+        Application specific context information
+    Return:
+        Decrypted payload
+    """
+    peer_public_key = serialization.load_pem_public_key(pub_pem, backend=default_backend())
+    secret = compute_ecdh_secret(ec_key, peer_public_key)
+    ckdf = ConcatKDFHash(
+        algorithm=hashes.SHA256(), length=32, otherinfo=other_info, backend=default_backend())
+    key = ckdf.derive(secret)
+    plaintext = decrypt_aes_gcm(key, payload[:12], payload[12:])
+    return plaintext
+
+def sign_ecdsa(ec_key, message):
+    """ Use ECDSA to sign message
+    Args:
+        My private key
+        Message to sign
+    Return:
+        signature
+    """
+    signer = ec_key.signer(ec.ECDSA(hashes.SHA256()))
+    signer.update(message)
+    signature = signer.finalize()
+    return signature
+
+def verify_ecdsa(ec_key, signature, message):
+    """ Use ECDSA to verify signed message
+    Args:
+        Peer's public key
+        Signature of message
+        Message to verify
+    Return:
+        true if verification succeeds
+    """
+    try:
+        verifier = ec_key.verifier(signature, ec.ECDSA(hashes.SHA256()))
+        verifier.update(message)
+        ret = verifier.verify()
+        return ret
+    except InvalidSignature as inst:
+        print inst
+        return False
 
 def hash_sha1(message):
     """ Hash using SHA-1
