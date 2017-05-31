69f1c3b1082220a41abe65732633a6dcba7bf033
Alan Frost <frosty.alan@gmail.com>
2017-05-30 17:02:48 -0700

    Recipe handling updates

--- recipe.py
+++ recipe.py
@@ -10,8 +10,8 @@ Implementation of Recipe manage
 
 import simplejson as json
 
-#from awsutils import DynamoDB
-#RECIPES = DynamoDB('Recipes')
+from awsutils import DynamoDB
+from utils import generate_user_id, contains_only
 
 class RecipeManager(object):
     """ Recipe Manager
@@ -20,10 +20,12 @@ class RecipeManager(object):
     def __init__(self, website):
         self.website = website
         self.recipes = {}
+        self.database = DynamoDB('Recipes')
 
-    def load_recipe(self, infile):
-        """ Load json data for a recipe
-            {
+    def load_recipes(self, infile):
+        """ Load json data for recipes
+            [
+              {
               "title": "Korean Meatballs",
               "ingredients": {
                 "subtitle": "Meatballs",
@@ -44,44 +46,96 @@ class RecipeManager(object):
               },
               "instructions": [
                 "preheat over to 400, line baking sheet with parchment",
-                "in a large bowl mix meatball ingredients, form into 1\" balls, cook for 20-25 minutes",
-                "meanwhile in medium bowl mix glaze ingredients, add meatballs and toss until coated",
+                "in a large bowl mix meatball ingredients, form into 1\" balls, cook 20-25 minutes",
+                "in medium bowl mix glaze ingredients, add meatballs and toss until coated",
                 "garnish with chopped chives or green ends of onions",
                 "serve over noodles or rice:
               ]
-            }
+            }, 
+            ...
+            ]
         Args:
             file: json file to load
         """
         try:
             with open(infile) as json_file:
-                recipe = json.load(json_file)
-                if 'title' in recipe and 'ingredients' in recipe and 'instructions' in recipe:
-                    html = self.render_recipe(recipe)
-                    if html is not None:
-                        print "Loaded " + recipe['title']
-                        return html
+                recipes = json.load(json_file)
+                for recipe in recipes:
+                    if 'title' in recipe and 'ingredients' in recipe and 'instructions' in recipe:
+                        html = self.render_recipe(recipe)
+                        if html is not None:
+                            recipe_id = generate_user_id(recipe['title'])
+                            print "Loaded " + recipe['title']
+                            self.recipes[recipe_id] = html
         except IOError:
             print 'Load of recipe failed: ' + infile
 
     def get_recipe(self, recipe_id):
-        """ Load json data for a recipe
+        """ Load recipe from Database
+        Args:
+            recipe_id: Database 'id' or title
+        Returns:
+            dictionary
         """
+        if len(recipe_id) == 48 and contains_only(recipe_id, '0123456789ABCDEFGHJKMNPQRSTVWXYZ'):
+            return self.database.get_item('id', recipe_id)
+        else:
+            return self.database.get_item('id', generate_user_id(recipe_id))
+
+    def save_recipe(self, recipe):
+        """ Save recipe in Database
+        Args:
+            recipe: Dictionary
+        Returns:
+            dictionary status
+        """
+        if 'title' in recipe:
+            recipe_id = generate_user_id(recipe['title'])
+            recipe['id'] = recipe_id
+            return self.database.put_item('id', recipe)
+        else:
+            return dict(error='Missing recipe title')
 
     def render_ingredients(self, ingredients):
+        """ Render recipe ingredients as HTML
+        Args:
+            ingredients: dictionary
+        Returns:
+            HTML
+        """
+
         if 'title' in ingredients:
             html = '<h5>' + ingredients['title'] + '</h5>\n<ul>\n'
+        else:
+            html = '<ul>\n'
 
         index = 1
         while 'item' + str(index) in ingredients:
             item = ingredients.get('item' + str(index))
             quantity = item.get('quantity')
-            quantity = quantity.replace('1/8', '&#x215B;')
-            quantity = quantity.replace('1/4', '&frac14;')
-            quantity = quantity.replace('1/2', '&frac12;')
-            quantity = quantity.replace('3/4', '&frac34;')
-            quantity = quantity.replace('1/3', '&#x2153;')
-            quantity = quantity.replace('2/3', '&#x2154;')
+            fraction = quantity.find('/')
+            if fraction != -1:
+                if quantity[fraction + 1] == '2':
+                    quantity = quantity.replace('1/2', '&frac12;')
+                elif quantity[fraction + 1] == '4':
+                    if quantity[fraction - 1] == '1':
+                        quantity = quantity.replace('1/4', '&frac14;')
+                    else:
+                        quantity = quantity.replace('3/4', '&frac34;')
+                elif quantity[fraction + 1] == '3':
+                    if quantity[fraction - 1] == '1':
+                        quantity = quantity.replace('1/3', '&#8531;')
+                    else:
+                        quantity = quantity.replace('2/3', '&#8532;')
+                elif quantity[fraction + 1] == '8':
+                    if quantity[fraction - 1] == '1':
+                        quantity = quantity.replace('1/8', '&#8539;')
+                    elif quantity[fraction - 1] == '3':
+                        quantity = quantity.replace('3/8', '&#8540;')
+                    elif quantity[fraction - 1] == '5':
+                        quantity = quantity.replace('5/8', '&#8541;')
+                    else:
+                        quantity = quantity.replace('7/8', '&#8542;')
             html += '  <li>' + quantity + ' ' + item.get('ingredient') + '</li>\n'
             index += 1
 
@@ -104,17 +158,20 @@ class RecipeManager(object):
             html += self.render_ingredients(ingredients['section1'])
             if 'section2' in ingredients:
                 html += self.render_ingredients(ingredients['section2'])
+            if 'section3' in ingredients:
+                html += self.render_ingredients(ingredients['section3'])
         else:
             html += self.render_ingredients(ingredients)
         html += '<h5><i class="fa fa-snowflake-o" aria-hidden="true"></i> Instructions</h5>\n'
         if mode == 'make':
-            html+= '<ol>\n'
+            html += '<ol>\n'
         else:
-            html+= '<p>\n'
+            html += '<p>\n'
         index = 1
         instructions = recipe.get('instructions')
         while 'step' + str(index) in instructions:
             item = instructions.get('step' + str(index))
+            item = item.replace('degrees', '&#8457;')
             if mode == 'make':
                 html += '  <li>' + item + '</li>\n'
             else:
@@ -126,11 +183,25 @@ class RecipeManager(object):
             html += '</p>\n'
         return html
 
+    def get_rendered_recipe(self, recipe_id):
+        print recipe_id
+        if len(recipe_id) != 48 or not contains_only(recipe_id, '0123456789ABCDEFGHJKMNPQRSTVWXYZ'):
+            recipe_id = generate_user_id(recipe_id)
+        if recipe_id in self.recipes:
+            return self.recipes[recipe_id]
+        else:
+            recipe = self.get_recipe(recipe_id)
+            if recipe is not None:
+                return render_recipe(recipe)
+
 def main():
     """ Unit tests
     """
     manager = RecipeManager('noneedtomeasure')
-    manager.load_recipe('recipes.json')
+    manager.load_recipes('recipes.json')
+    print manager.get_rendered_recipe('Korean Meatballs')
+    print manager.get_rendered_recipe('Pumpkin Waffles')
+    print manager.get_rendered_recipe('Strawberry Pancakes')
 
 if __name__ == '__main__':
     main()
