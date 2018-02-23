481a42cba96c53e255bb1106bf48882ab985efd6
Alan Frost <frosty.alan@gmail.com>
2018-02-23 07:58:43 -0800

    Disable autoscroll, hide play/pause button, generate search list

--- tools/gallery.py
+++ tools/gallery.py
@@ -28,7 +28,7 @@ def parse_options():
     parser.add_argument('-p', '--page', action='store', default='gallery.html')
     parser.add_argument('-s', '--site', action='store', default='https://susanafrost.com')
     parser.add_argument('-t', '--title', action='store', default='Susan Frost')
-    parser.add_argument('command', action='store', help='check, csv, gallery, masonary')
+    parser.add_argument('command', action='store', help='check, csv, gallery, masonary, search')
     return parser.parse_args()
 
 def load_images(images_file):
@@ -46,7 +46,7 @@ def load_images(images_file):
     return images
 
 def read_csv(csv_file):
-    """ Read a CSV file with art piece metadata
+    """ Read a CSV file with visual art work metadata
     Args:
         csv filename
     Return:
@@ -61,7 +61,7 @@ def read_csv(csv_file):
         return csv_rows
 
 def write_csv(images):
-    """ Write a CSV file with art piece metadata
+    """ Write a CSV file with visual art work metadata
     Args:
         images dict
     """
@@ -72,7 +72,8 @@ def write_csv(images):
         writer.writerow(image)
 
 def check_images(images):
-    """ Check an images dict for required art piece metadata
+    """ Check an images dict for required visual art work metadata. See
+        http://schema.org/VisualArtwork for all sorts of metadata that could be added.
     Args:
         images dict
     """
@@ -156,6 +157,16 @@ def generate_gallery_indicators(images, medium='*'):
         html += '       </ol>\n'
     print(html)
 
+def generate_search_list(images):
+    slide = 0
+    html = ''
+    for image in images:
+        title = image['title']
+        thumb = image['image'].replace('.jpg', '_thumb.jpg')
+        html += '<li><a href="gallery.html?slide=' + str(slide) + '">' + title + '<img src="static/img/' + thumb + '"></a></li>\n'
+        slide += 1
+    print(html)
+
 def generate_gallery(site, images, medium='*'):
     """ Generate HTML gallery layput
     Args:
@@ -169,7 +180,7 @@ def generate_gallery(site, images, medium='*'):
 
     html = '  <section class="slide-wrapper">\n'
     html += '    <div class="container">\n'
-    html += '      <div id="artCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="6000">\n'
+    html += '      <div id="artCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="false">\n'
     print(html)
     generate_gallery_indicators(images, medium)
     html = '        <!-- Wrapper for slides -->\n'
@@ -189,16 +200,18 @@ def generate_gallery(site, images, medium='*'):
         html += '            <div class="fill" style=" background-color:#48c3af;">\n'
         html += '              <div class="inner-content">\n'
         html += '                <div class="carousel-img">\n'
+        html += '                <a href="' + img_path + img_hd + '">\n'
         html += '                <picture>\n'
         html += '                  <source srcset="' + img_path + img_hd + '" media="(min-width: 1400px)">\n'
         html += '                  <source srcset="' + img_path + img_med + '" media="(min-width: 768px)">\n'
         html += '                  <source srcset="' + img_path + img_small + '" media="(min-width: 576px)">\n'
         html += '                  <img srcset="' + img_path + img_small + '" alt="responsive image" class="img img-responsive">\n'
         html += '                </picture>\n'
+        html += '                </a>\n'
         #html += '                  <img src="' + site + 'static/img/' + img_hd + '" alt="' + title + '" class="img img-responsive" />\n'
         html += '                </div>\n'
         html += '                <div class="carousel-desc">\n'
-        html += '                  <h2>' + title + '</h2>\n'
+        html += '                  <h3>' + title + '</h3>\n'
         html += '                  <p>' + image['medium'] + ', ' + image['size'] + ', ' + image['created'] + '</p>\n'
         html += '                </div>\n'
         html += '              </div>\n'
@@ -216,11 +229,11 @@ def generate_gallery(site, images, medium='*'):
     html += '            <span class="fa fa-chevron-right"></span>\n'
     html += '            <span class="sr-only">Next</span>\n'
     html += '        </a>\n'
-    html += '        <div id="carouselButtons">\n'
-    html += '          <button id="playButton" type="button" class="btn btn-default btn-xs">\n'
-    html += '            <span class="fa fa-play"></span>\n'
-    html += '          </button>\n'
-    html += '        </div>\n'
+    #html += '        <div id="carouselButtons">\n'
+    #html += '          <button id="playButton" type="button" class="btn btn-default btn-xs">\n'
+    #html += '            <span class="fa fa-play"></span>\n'
+    #html += '          </button>\n'
+    #html += '        </div>\n'
     html += '      </div>\n'
     html += '    </div>\n'
     html += '  </section>\n'
@@ -252,6 +265,8 @@ def main():
     if images is None:
         print('No images loaded')
         return
+    else:
+        pass #print('Loaded {} images'.format(len(images)))
     site = options.site
     if len(site) > 1:
         site += '/'
@@ -265,6 +280,8 @@ def main():
         generate_masonary_page(site, options.page, options.title, images, options.medium)
     elif options.command == 'gallery':
         generate_gallery_page(site, options.page, options.title, images, options.medium)
+    elif options.command == 'search':
+        generate_search_list(images)
 
 if __name__ == '__main__':
     main()
