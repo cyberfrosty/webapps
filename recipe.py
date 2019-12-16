38772aa646945151e6effc0f13fd18d3ebc152f9
Alan Frost <alan@ionu.com>
2019-12-16 13:47:02 -0700

    Lamb Kofta

--- recipe.py
+++ recipe.py
@@ -19,7 +19,7 @@ from utils import generate_id, contains_only, read_csv, compare_dicts
 
 TBSP2CUP = 0.0625
 TSP2CUP = 0.020833
-latest = ['Vietnamese Meatballs', 'Korean Meatball Marinara', 'Parmesan Roasted Brussel Sprouts', 'Whole Wheat Biscuits', 'Cashew Chicken', 'Gingerbread Scones', 'Sweet and Spicy Shrimp', 'Balsamic Tomato Chicken', 'Tomato Carrot Chicken']
+latest = ['Lamb Kofta', 'Vietnamese Meatballs', 'Korean Meatball Marinara', 'Parmesan Roasted Brussel Sprouts', 'Whole Wheat Biscuits', 'Cashew Chicken', 'Gingerbread Scones', 'Sweet and Spicy Shrimp', 'Balsamic Tomato Chicken', 'Tomato Carrot Chicken']
 
 def render_ingredients(ingredients):
     """ Render recipe ingredients as HTML
